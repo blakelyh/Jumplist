295514ec8634129823a5d2a836d1f0c2fe6d9b14
Hunter Blakely <blakelyh@sou.edu>
2013-02-09 12:11:10 -0800

    first commit

---
+++ src/VolatilityPlugin/jumpList.py
@@ -0,0 +1,44 @@
+# Volatility
+# Copyright (C) 2008 Volatile Systems
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the license, or (at
+# your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+
+from vutils import *
+
+
+// Jump List Enumeration
+class jle():
+
+	# Declare meta information associated with this plugin
+	
+	# meta_info = blah.commands.command.meta_info
+	# meta_info['author'] = 'Hunter Blakely, John Carlson'
+	# meta_info['copyright'] = 'Copyright (c) 2013 Hunter Blakely, John Carlson'
+	# meta_info['contact'] = 'blakelyh@sou.edu'
+	# meta_info['url'] = 'https://github.com/blakelyh/Jumplist'
+	# meta_info['os'] = 'Win7-Win8'
+	# meta_info['version'] = 'beta'
+	
+
+	# def parseMS_CFB():
+		# return parsedInfo
+
+	# def parseMS_SHLLINK():
+		# return parsedInfo
+
+	# def DecryptJumpListID():
+		# return nameOfJumpList
+
