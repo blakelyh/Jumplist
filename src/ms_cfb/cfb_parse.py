07055faccf5b9d89a6f5c583b7bb1e0abe2bd020
System Administrator <root@IT-Administrators-MacBook-Pro.local>
2013-03-25 02:05:34 -0700

    Added byte order reversing

--- src/ms_cfb/cfb_parse.py
+++ src/ms_cfb/cfb_parse.py
@@ -1,66 +1,50 @@
-import struct
 import binascii
 import optparse
 
 def header(inFile):
-	sig=seekAndRead(inFile, 0x0000,0x08) 		# Header signature
-	clsid=seekAndRead(inFile, 0x0008,0x10)		# Header CLSID
-	minVersion=seekAndRead(inFile,0x0018,0x02)	# Minor version
-	majVersion=seekAndRead(inFile,0x001A,0x02) 	# Major version
-	byteOrder=seekAndRead(inFile,0x001C,0x02) 	# Byte order (little/big endian)
-	sectSize=seekAndRead(inFile,0x001E,0x02) 	# Sector Size
-	mStreamSectSize=seekAndRead(inFile,0x0020,0x02) # Mini stream sector size
-	res=seekAndRead(inFile,0x0022,0x06) 		# Reserved
-	nDirSect=seekAndRead(inFile,0x0028,0x04) 	# Number of directory sectors
-	nFATSect=seekAndRead(inFile,0x002c,0x04)	# Number of FAT sectors
-	dirStartSectLoc=seekAndRead(inFile,0x0030,0x04) # Directory start sector location
-	tSig=seekAndRead(inFile,0x0034,0x04) 		# Transaction signature
-	mStreamSizeCutoff=seekAndRead(inFile,0x0038,0x04) # Mini stream size cutoff
-	mFATStartSectLoc=seekAndRead(inFile,0x003C,0x04) # Mini FAT start sector location
-	nMFATSect=seekAndRead(inFile,0x0040,0x04) 	# Number of mini FAT sectors
-	DIFATStartSectLoc=seekAndRead(inFile,0x0044,0x04) # DIFAT start sector location
-	nDIFATSect=seekAndRead(inFile,0x0048,0x04) 	# Number of DIFAT sectors
+	head=18*[None]
+	head[0]=seekAndRead(inFile, 0x0000,0x08) 	# Header signature
+	head[1]=seekAndRead(inFile, 0x0008,0x10)	# Header CLSID
+	head[2]=seekAndRead(inFile,0x0018,0x02)		# Minor version
+	head[3]=seekAndRead(inFile,0x001A,0x02) 	# Major version
+	head[4]=seekAndRead(inFile,0x001C,0x02) 	# Byte order (little/big endian)
+	head[5]=seekAndRead(inFile,0x001E,0x02) 	# Sector Size
+	head[6]=seekAndRead(inFile,0x0020,0x02) 	# Mini stream sector size
+	head[7]=seekAndRead(inFile,0x0022,0x06) 	# Reserved
+	head[8]=seekAndRead(inFile,0x0028,0x04) 	# Number of directory sectors
+	head[9]=seekAndRead(inFile,0x002c,0x04)		# Number of FAT sectors
+	head[10]=seekAndRead(inFile,0x0030,0x04)	# Directory start sector location
+	head[11]=seekAndRead(inFile,0x0034,0x04)	# Transaction signature
+	head[12]=seekAndRead(inFile,0x0038,0x04)	# Mini stream size cutoff
+	head[13]=seekAndRead(inFile,0x003C,0x04)	# Mini FAT start sector location
+	head[14]=seekAndRead(inFile,0x0040,0x04)	# Number of mini FAT sectors
+	head[15]=seekAndRead(inFile,0x0044,0x04)	# DIFAT start sector location
+	head[16]=seekAndRead(inFile,0x0048,0x04)	# Number of DIFAT sectors
 	DIFAT=109*[None]
-	###POPULATE DIFAT
-	try:
-		#Testing purposes print("DIFAT PARSING\n\t\t0 1 2 3 4 5 6 7 8 9"+\
-		# " A B C D E F 0 1 2 3 4 5 6 7 8 9 A B C D E F\n")
+	if (int(head[7],16)!=0):
+		print (str(inFile)+" IS NOT A JUMP LIST!")
+		exit(0)			
+	if (str(head[4]) == "feff"):
+		for index, object in enumerate(head):
+			if (type(object)== str):
+				try:
+					head[index]=hex(revByteOrd(long(object,16)))		
+				except Exception, e:
+					print e
+	if(int(head[15],16)<int("0xfffffffa",16)):
 		c = 0	
 		for i in range(0, len(DIFAT)):
-			offset = int(DIFATStartSectLoc,16)+c
+			offset = int(head[15],16)+c
 			DIFAT[i]=seekAndRead(inFile,offset,32)
-			#Testing Purposes print (str(hex(offset))+"\t"+str(DIFAT[i]))
 			c += 32
-	except Exception, e:
-		print("ERROR PARSING DIFAT "+str(e)+"\n")
-	
-	# TESTING PURPOSES
-	print("IN HEADER FUNCTION")
-	print("\n\nOFFSET\t\t\t\t\t0 1 2 3 4 5 6 7 8 9 "+\
-	"A B C D E F 0 1 2 3 4 5 6 7 8 9 A B C D E F\n\n"+\
-		"\nSignature\t\t\t\t"+sig+\
-		"\nCLSID\t\t\t\t\t"+clsid+\
-		"\nMinor Version\t\t\t\t"+minVersion+\
-		"\nMajor Version\t\t\t\t"+majVersion+\
-		"\nByte Order\t\t\t\t"+byteOrder+\
-		"\nSector Size\t\t\t\t"+sectSize+\
-		"\nMini Stream Sector Size\t\t\t"+mStreamSectSize+\
-		"\nReserved\t\t\t\t"+res+\
-		"\nNumber of Directory sectors\t\t"+nDirSect+\
-		"\nNumber of FAT sectors\t\t\t"+nFATSect+\
-		"\nDirectory start sector location\t\t"+dirStartSectLoc+\
-		"\nTransaction signature\t\t\t"+tSig+\
-		"\nMini stream size cutoff\t\t\t"+mStreamSizeCutoff+\
-		"\nMini FAT start sector location\t\t"+mFATStartSectLoc+\
-		"\nNumber of mini FAT sectors\t\t"+nMFATSect+\
-		"\nDIFAT Start Sector Loccation\t\t"+DIFATStartSectLoc+\
-		"\nNumber of DIFAT sectors\t\t\t"+nDIFATSect)	
-		
-	if (str(byteOrder) == "feff"):
-		print("\tByte Order = Little Endian, must flip bytes")
+	if (str(head[4]) == "0xfffe"):
+		for i, o in enumerate(DIFAT):
+			if (o!="")and(o!=None):
+				DIFAT[i]=hex(revByteOrd(long(o,16)))
+	head[17]=DIFAT					# DIFAT Array
+	return head
 
 def fat(inFile, version, nFATSect, sectSize):
-	print("IN FAT SECTOR FUNCTION")
 	if (nFATSect>0):
 		start=sectSize
 		stepL=0x04
@@ -69,52 +53,41 @@ def fat(inFile, version, nFATSect, sectSize):
 		c = 0
 		for i in range (start,end,stepL):
 			nxtSect[c]=seekAndRead(inFile,i,stepL)
-			#print("nxtSect["+str(c)+"]:\t"+str(nxtSect[c])+"\n")
 			c += 1
-	# FOR TESTING
-	#print("\nFile Name\t\t"+inFile+\
-	#"\nVersion\t\t\t"+str(int(version))+\
-	#"\nNumber of FAT Sectors\t"+str(int(nFATSect))+\
-	#"\nStart\t\t\t"+str(hex(start))+\
-	#"\nStep size\t\t"+str(hex(stepL))+\
-	#"\nStep x nSect\t\t"+str(hex(int(stepL)*int(nFATSect)))+\
-	#"\nEnd\t\t\t"+str(hex(end)))
-
+		return nxtSect		
 
 def dir(inFile, version, dirStartSectLoc, sectSize, nDirSect):
-	print("IN DIRECTORY SECTOR FUNCTION")
 	sectSize = 2**(int(sectSize))
 	start = int(dirStartSectLoc)*sectSize+sectSize
+	d=12*[None]
 	if(nDirSect>0):
-		dirEntryName=seekAndRead(inFile,start,64) 
-		start +=64; #print("DirEntryN\t"+str(dirEntryName)) 
-		dirEntryNameLen=seekAndRead(inFile,start,2)
-		start +=2; #print("dirEntryNL\t"+str(dirEntryNameLen))
-		objType=seekAndRead(inFile,start,1)
-		start +=1; #print("objType\t\t"+str(objType))
-		cFlag=seekAndRead(inFile,start,1)
-		start +=1; #print("color flag\t"+str(cFlag))
-		lSID=seekAndRead(inFile,start,4)
-		start +=4; #print("lSiblingID\t"+str(lSID))
-		rSID=seekAndRead(inFile,start,4)
-		start +=4; #print("rSiblingID\t"+str(rSID))
-		childID=seekAndRead(inFile,start,4)
-		start +=4; #print("child ID\t"+str(childID))
-		clsid=seekAndRead(inFile,start,16)
-		start +=16; #print("clsid\t\t"+str(clsid))
-		sFlags=seekAndRead(inFile,start,4)
-		start +=4; #print("sFlags\t\t"+str(sFlags))
-		cTime=seekAndRead(inFile,start,8)
-		start +=8; #print("cTime\t\t"+str(cTime))
-		modTime=seekAndRead(inFile,start,8)
-		start +=8; #print("modTime\t\t"+str(modTime))
-		sSectLoc=seekAndRead(inFile,start,4)
-		#print("sSectLoc\t"+str(sSectLoc))
-	# FOR TESTING
-	print("\tmust iterate through all dir sectors")
+		d[0]=dirEntryName=seekAndRead(inFile,start,64) 
+		start +=64;  
+		d[1]=dirEntryNameLen=seekAndRead(inFile,start,2)
+		start +=2; 
+		d[2]=objType=seekAndRead(inFile,start,1)
+		start +=1; 
+		d[3]=cFlag=seekAndRead(inFile,start,1)
+		start +=1; 
+		d[4]=lSID=seekAndRead(inFile,start,4)
+		start +=4; 
+		d[5]=rSID=seekAndRead(inFile,start,4)
+		start +=4; 
+		d[6]=childID=seekAndRead(inFile,start,4)
+		start +=4; 
+		d[7]=clsid=seekAndRead(inFile,start,16)
+		start +=16; 
+		d[8]=sFlags=seekAndRead(inFile,start,4)
+		start +=4; 
+		d[9]=cTime=seekAndRead(inFile,start,8)
+		start +=8; 
+		d[10]=modTime=seekAndRead(inFile,start,8)
+		start +=8; 
+		d[11]=sSectLoc=seekAndRead(inFile,start,4)
+	print("\tMUST ITERATE FOR EACH DIRECTORY SECTOR")
+	return d
 
 def mFAT(inFile, sectSize, mSSCutoff, mFATStartSectLoc, nMFATSect):
-	print("IN MINI FAT SECTOR FUNCTION")
 	if(nMFATSect>0):
 		sSize=2**(int(sectSize))
 		start=int(mFATStartSectLoc)*sSize+sSize
@@ -124,9 +97,9 @@ def mFAT(inFile, sectSize, mSSCutoff, mFATStartSectLoc, nMFATSect):
 		c=0
 		for i in range(start,end,stepL):
 			nxtSect[c]=seekAndRead(inFile,i,stepL)
-			#print("Offset "+str(hex(i))+"\tVal "+str(nxtSect[c]))
 			c+=1
 		print("\tMUST ITERATE FOR EACH MINI FAT SECTOR")
+		return nxtSect
 
 def seekAndRead(inFile, bOffset, bLength):
 	try:
@@ -140,6 +113,27 @@ def seekAndRead(inFile, bOffset, bLength):
 	out = f.read(bLength).encode("hex")
 	f.close()
 	return out 
+
+def revByteOrd(data):
+	s = "Error: Only 'unsigned' data of type 'int' or 'long' is allowed"
+	if not ((type(data) == int)or(type(data) == long)):
+		s1 = "Error: Invalid data type: %s" % type(data)
+		print(''.join([s,'\n',s1]))
+    		return data
+	if(data < 0):
+		s2 = "Error: Data is signed. Value is less than 0"
+		print(''.join([s,'\n',s2]))
+		return data
+	seq = ["0x"]
+	if data==0:
+		return data
+	else:
+		while(data > 0):
+			d = data & 0xFF     # extract the least significant(LS) byte
+			seq.append('%02x'%d)# convert to appropriate string, append to sequence
+			data >>= 8          # push next higher byte to LS position
+			revD = int(''.join(seq),16)
+	return revD
 
 def main():
 	parser = optparse.OptionParser('usage%prog '+\
@@ -173,12 +167,12 @@ def main():
 		except Exception, e:
 			print("Error: " + str(e))
 	# header(inFile)
-	header(inFile)
+	h = header(inFile)
 	# fat(inFile, version, nFATSect, sectSize)
-	fat(inFile, 0x0003, 0x000001, 0x0200)
+	f = fat(inFile, 0x0003, 0x000001, 0x0200)
 	# dir(inFile, version, dirstartSectLoc, sectSize, nDirSect)
-	dir(inFile, 0x0003, 0x00000001, 0x0009, 0x2800)
+	d = dir(inFile, 0x0003, 0x00000001, 0x0009, 0x2800)
 	# mFAT(iFile, sectSize, mSSCutoff, mFATStartSectLoc, nMFATSect)
-	mFAT(inFile, 0x0009, 0x00001000, 0x00000002, 0x00000001)
+	m = mFAT(inFile, 0x0009, 0x00001000, 0x00000002, 0x00000001)
 if __name__ == '__main__':
 	main()
