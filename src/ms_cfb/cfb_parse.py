c4b562f6e39b638c47e7a8b06dd65f25a4ddfc0d
Hunter Blakely <blakelyh@sou.edu>
2013-05-15 10:27:02 -0700

    filling in progMatch

--- src/ms_cfb/cfb_parse.py
+++ src/ms_cfb/cfb_parse.py
@@ -21,7 +21,7 @@ def header(inFile):
 	head[15]=seekAndRead(inFile,0x0044,0x04)	# DIFAT start sector location
 	head[16]=seekAndRead(inFile,0x0048,0x04)	# Number of DIFAT sectors
 	DIFAT=109*[None]
-	if (int(head[7],16)!=0):					# Verify inFIle is Jump List
+	if (int(head[7],16)!=0):				# Verify inFIle is Jump List
 		print (str(inFile)+" IS NOT A JUMP LIST!")
 		exit(0)					
 	if (str(head[4]) == "feff"):				# Determine Byte Order
@@ -31,7 +31,7 @@ def header(inFile):
 					head[index]=revByteOrd(long(object,16))
 				except Exception, e:
 					print e
-	if(int(head[15],16)<int("0xfffffffa",16)):	# DIFAT REG_SECT check
+	if(int(head[15],16)<int("0xfffffffa",16)):		# DIFAT REG_SECT check
 		c = 0	
 		for i in range(0, len(DIFAT)):
 			offset = int(head[15],16)+c
@@ -41,22 +41,22 @@ def header(inFile):
 		for i, o in enumerate(DIFAT):
 			if (o!="")and(o!=None):
 				DIFAT[i]=revByteOrd(long(o,16))
-	head[17]=DIFAT								# DIFAT Array
+	head[17]=DIFAT						# DIFAT Array
 	return head					
 
 def fat(inFile, version, sectSize, nFATSect):
 	stepL=0x04                              	# Step length  
 	start=sSize=2**(int(str(sectSize),16))		# sSize offset
-	end=start+sSize	         					# End of FAT
-	f=int(nFATSect,16)*[None]					# FAT sectors Array
+	end=start+sSize	         			# End of FAT
+	f=int(nFATSect,16)*[None]			# FAT sectors Array
 	for index in range(0,int(nFATSect,16)):
-		nxtSect=sSize/int(stepL)*[None] 		# nxtSect Array
-		c = 0 									# index for nxtSect
-		if(int(version,16) == 3):				# Version 3, revByteOrd
+		nxtSect=sSize/int(stepL)*[None] 	# nxtSect Array
+		c = 0 					# index for nxtSect
+		if(int(version,16) == 3):		# Version 3, revByteOrd
 			for i in range (start,end,stepL):
 				nxtSect[c]=revByteOrd(long(seekAndRead(inFile,i,stepL),16))
 				c += 1
-		else:									# Not version 3
+		else:					# Not version 3
 			for i in range (start,end,stepL):
 				nxtSect[c]=seekAndRead(inFile,i,stepL)
 				c += 1
@@ -64,31 +64,31 @@ def fat(inFile, version, sectSize, nFATSect):
  	return f
 
 def dir(inFile, version, sectSize, dirStartSectLoc, nDirSect):
-	sSize = 2**(int(sectSize,16))					# size is 2^sectSize
+	sSize = 2**(int(sectSize,16))				# size is 2^sectSize
 	start = int(dirStartSectLoc,16)*sSize+sSize		# start location 
 	# Fill Array[index] with fully populated d[].
 	if (int(version,16)==3 and int(nDirSect)!=0):
 			print str(inFile)+" is corrupt, or is not a jump list."
 			exit(0)
 	elif (int(version,16)==3):
-		number = 1 									# number is nDirSect
+		number = 1 					# number is nDirSect
 	else:
 		number = int(nDirSect,16)
 	di=number*[None]
 	for index in range(0,number):
 		d=12*[None]										# Dir Array
 		d[0]=seekAndRead(inFile,start,64);start +=64  	# Dir Entry Name
-		d[1]=seekAndRead(inFile,start,2);start +=2		# Dir Entry Name Length
-		d[2]=seekAndRead(inFile,start,1);start +=1		# objType
-		d[3]=seekAndRead(inFile,start,1);start +=1		# cFlag
-		d[4]=seekAndRead(inFile,start,4);start +=4		# lSID
-		d[5]=seekAndRead(inFile,start,4);start +=4		# rSID
-		d[6]=seekAndRead(inFile,start,4);start +=4		# childID
+		d[1]=seekAndRead(inFile,start,2);start +=2	# Dir Entry Name Length
+		d[2]=seekAndRead(inFile,start,1);start +=1	# objType
+		d[3]=seekAndRead(inFile,start,1);start +=1	# cFlag
+		d[4]=seekAndRead(inFile,start,4);start +=4	# lSID
+		d[5]=seekAndRead(inFile,start,4);start +=4	# rSID
+		d[6]=seekAndRead(inFile,start,4);start +=4	# childID
 		d[7]=seekAndRead(inFile,start,16);start +=16	# clsid
-		d[8]=seekAndRead(inFile,start,4);start +=4		# sFlags
-		d[9]=seekAndRead(inFile,start,8);start +=8		# cTime
-		d[10]=seekAndRead(inFile,start,8);start +=8		# modTime
-		d[11]=seekAndRead(inFile,start,4)				# sSectLoc
+		d[8]=seekAndRead(inFile,start,4);start +=4	# sFlags
+		d[9]=seekAndRead(inFile,start,8);start +=8	# cTime
+		d[10]=seekAndRead(inFile,start,8);start +=8	# modTime
+		d[11]=seekAndRead(inFile,start,4)		# sSectLoc
 		if (int(version,16)==3):
 			for i in range (len(d)):
 				d[i]=revByteOrd(long(d[i],16))
@@ -118,15 +118,15 @@ def mFAT(inFile, version, sectSize, mSSCutoff, mFATStartSectLoc, nMFATSect):
 
 def seekAndRead(inFile, bOffset, bLength):
 	try:
-		f = open(inFile, 'r')						# Open file
+		f = open(inFile, 'r')				# Open file
 	except Exception, e:
 		print("Error opening <"+inFile+">: "+str(e))
 	try:
-		f.seek(bOffset)								# Seek to offset
+		f.seek(bOffset)					# Seek to offset
 	except Exception, e:
 		print("Error seeking to "+str(bOffset)+" "+str(e))
-	out = f.read(bLength).encode("hex")				# Read for length
-	f.close()										# Close file
+	out = f.read(bLength).encode("hex")			# Read for length
+	f.close()						# Close file
 	return out 
 
 def revByteOrd(data):
@@ -155,15 +155,26 @@ def progMatch(file):
 	# for each line in file switch statement to match the programs to the list.
 	# create array of file names that match the associated program.	
 	# parse for each matched file.
-	print "progMatch"	
+	try:
+		with open(inFile, 'rb') as f:
+			content = f.read()				
+	except Exception, e:
+		print("Error: " + str(e))		
+	
+	# for each line in content, if string does not match ^: fill progs.
+	progs = len(file)*[None]
+	# fill progs with the appropriate file names corresponding to the program name
+	return progs
 
-
-def parse(data):
+def parseCFB(data):
 	# for each program/input file.
 	h = header(inFile)
 	f = fat(inFile, h[3], h[5], h[9])
 	d = dir(inFile, h[3], h[5], h[10], h[8])
 	m = mFAT(inFile, h[3], h[5], h[12], h[13], h[14])
+
+def parseSHLLINK(data):
+	print "write the code to parse shell-link"
 
 def main():
 	parser = optparse.OptionParser('\n\tusage%prog '+\
@@ -189,7 +200,7 @@ def main():
 	outFile = options.oFile
 
 	if pFile != None:
-		progMatch(pFile)
+		progs = progMatch(pFile)
 	elif inFile == None and outFile == None:
 		print parser.usage	
 		exit(0)
